f4f83272dfb7308b02ab06df0d0fcbd16035b99a
Nadeesani Navaratne <nadeesani.navarate@gmail.com>
2020-07-12 16:42:47 +0200

    Adding active class functionality for the links

--- js/app.js
+++ js/app.js
@@ -21,61 +21,29 @@ sectionIDs.forEach(section => {
 });
 
 navBar.style.cssText = 'height:35px; display:flex; justify-content:space-around; margin-top:10px';
-
-/*let navLinks =document.querySelectorAll('.scrollLink');
-console.log("Test");
-console.log(navLinks);
-
-let mainNavLinks = document.querySelectorAll('nav ul li a');
-console.log(mainNavLinks);
-let sec = document.querySelector(link.hash);
-console.log(sec);*/
-
 let menuNavLinks = document.getElementsByClassName('scrollLink');
-console.log("Menu");
-console.log(menuNavLinks);
-
-/*let menuNavLinks2= document.querySelectorAll('.scrollLink');
-console.log(menuNavLinks2);*/
 
 let menulist = document.getElementById('navbar__list');
-console.log("Menulist finddddddd");
-console.log(menulist);
-
 let elementsLi = document.querySelectorAll('ul#navbar__list > li a');
-console.log("elementsList ");
-console.log(elementsLi);
 
 //Adding active class to nav links
 
-/*document.getElementById('navbar__list').addEventListener("click", function(e) {
-
-    if (e.target && e.target.nodeName === "A") {
-        let elements = document.getElementById('navbar__list').children;
-        console.log("Elements");
-        console.log(elements);
-        for (let i = 0; i < elements.length; i++) {
-            elements[i].classList.remove("active-class");
-        }
-        
-        e.target.classList.add("active-class");
-        e.currentTarget.classList.add('active-class');
-        console.log("Test");
-     }
-}, true);*/
-
 for (let i = 0; i < elementsLi.length; i++) {
     elementsLi[i].addEventListener('click', function (e) {
-        // console.log(e.currentTarget); // for see the current element
         if (e.currentTarget.classList.contains('active-class')) {
             // do something if already have class "active-class"
             console.log("Test");
         }
         else {
+            for (let j = 0; j < elementsLi.length; j++) {
+                elementsLi[j].classList.remove("active-class");
+            }
             e.currentTarget.classList.add('active-class');
         }
+
     });
 }
+
 
 //Scroll function for active section
 sectionIDs.forEach((section) =>
